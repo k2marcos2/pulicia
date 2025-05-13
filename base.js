cc8c986ece2e7d731eb9f9e3c4b68db61299863e
Marcos Brito Feitosa <marcosfeitosapalmas2@gmail.com>
2025-05-12 21:02:58 -0300

    first commit

--- base.js
+++ base.js
@@ -1,35 +1,36 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const form = document.querySelector("form");
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('checklist-form');
 
-    form.addEventListener("submit", async function (e) {
-        e.preventDefault();
+  form.addEventListener('submit', async (e) => {
+    e.preventDefault();
 
-        const formData = new FormData(form);
-        const data = {};
+    const formData = new FormData(form);
+    const data = {};
 
-        formData.forEach((value, key) => {
-            if (data[key]) {
-                data[key] += `, ${value}`; // Agrupa valores de checkboxes com o mesmo nome
-            } else {
-                data[key] = value;
-            }
-        });
-
-        try {
-            await fetch("https://script.google.com/macros/s/AKfycbws5SEJCXlXydZbYcxwnzAYjBvJzCYrK4OeBgpmtNKFem8P3WT9im-YBKGk5N3f2CMN_g/exec", {
-                method: "POST",
-                mode: "no-cors",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(data)
-            });
-
-            alert("Formulário enviado com sucesso!");
-            form.reset();
-        } catch (error) {
-            console.error("Erro ao enviar:", error);
-            alert("Erro ao enviar o formulário.");
-        }
+    // Agrupa múltiplos checkboxes
+    formData.forEach((value, key) => {
+      if (data[key]) {
+        data[key] += `, ${value}`;
+      } else {
+        data[key] = value;
+      }
     });
+
+    try {
+      const response = await fetch("https://script.google.com/macros/s/AKfycbyGtwQAQrg2lnVVT2QMf6DkYG_ieFtezoaO1zuWQwiPr2HtueH8AQn0zquJIUYrvOPF-w/exec", {
+        method: "POST",
+        mode: "no-cors",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      alert("Formulário enviado com sucesso!");
+      form.reset();
+    } catch (error) {
+      console.error("Erro ao enviar:", error);
+      alert("Erro ao enviar o formulário.");
+    }
+  });
 });
